46febb84b99aafe03fd73c428703626feca82ab7
bart <bartolomeofalzarano@gmail.com>
2022-06-13 17:22:00 -0700

    cSlider - generated components and a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import {AppRoutingModule} from "./app-routing-module";
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { GroupPageComponent } from './pages/group-page/group-page.component';
+import { SliderComponent } from './pages/home-page/slider/slider.component';
+import { GroupSliderComponent } from './pages/home-page/slider/group-slider/group-slider.component';
+import { EventSliderComponent } from './pages/home-page/slider/event-slider/event-slider.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import { GroupPageComponent } from './pages/group-page/group-page.component';
     AboutPageComponent,
     LoginPageComponent,
     GroupPageComponent,
+    SliderComponent,
+    GroupSliderComponent,
+    EventSliderComponent,
   ],
   imports: [
     FormsModule,
