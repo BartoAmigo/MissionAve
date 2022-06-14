8ef843d4d5ec290883d704b149e749fbad197fd5
bart <bartolomeofalzarano@gmail.com>
2022-06-13 19:30:31 -0700

    group slider changes

--- src/app/pages/home-page/slider/group-slider/group-slider.component.ts
+++ src/app/pages/home-page/slider/group-slider/group-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-group-slider',
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-slider.component.css']
 })
 export class GroupSliderComponent implements OnInit {
-
-  constructor() { }
+  groups : string[] = ["Horse People", "Dog People"]; 
+  currentGroup = 0;
+  selectedGroup = this.groups[this.currentGroup]; 
+  constructor() { 
+  }
 
   ngOnInit(): void {
   }
+
+  nextGroup() {
+      if(this.currentGroup != this.groups.length-1) {
+      this.currentGroup++;
+      this.selectedGroup = this.groups[this.currentGroup];
+    }
+  } 
+
+  prevGroup() {
+    if(this.currentGroup != 0) {
+    this.currentGroup--;
+    this.selectedGroup = this.groups[this.currentGroup];
+  }
+}
 
 }
